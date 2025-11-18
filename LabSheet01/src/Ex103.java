cb695ae79c90ee6fe4fa35b596cda9a5e833a4ff
Khawpuntuna2 <02xoe16@gmail.com>
2025-11-18 13:53:04 +0700

    submit exercise LabSheet01

---
+++ LabSheet01/src/Ex103.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Ex103 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Hello, I am Suphawit Thawornaniwat\nI want grade A","Welcome",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
